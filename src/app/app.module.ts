72a1400c460dbab6cb6f2367416af991831f361c
sitek94 <msitkowski94@gmail.com>
2021-04-11 19:50:03 +0200

    feat: add routing and simple component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FirstLessonComponent } from './shared/components/first-lesson/first-lesson.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, FirstLessonComponent, FirstLessonComponent, HomeComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'first-lesson', component: FirstLessonComponent },
+      { path: '', component: HomeComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
