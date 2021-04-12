31fbe3d32fd39e9bd4a3aa6d6de6d0b708dc3c5a
sitek94 <msitkowski94@gmail.com>
2021-04-12 18:20:56 +0200

    feat(navbar): use `*ngFor` to iterate over links

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
+function createLink(name: string) {
+  return { name, url: `${name.toLocaleLowerCase().trim().replace(/\s/g, '-')}` };
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  links = [createLink('Home'), createLink('Test Page'), createLink('Tour of Heroes')];
+}
